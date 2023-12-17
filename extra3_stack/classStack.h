df8510f7c45a5769d50a2f07a27d17e140ba33a1
polinazharova <1132226165@pfur.ru>
2023-12-17 14:53:56 +0300

    optimized

---
+++ extra3_stack/classStack.h
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Stack {
+    //приватные атрибуты
+private:
+    //класс узлов стека
+    class Node {
+    private:
+        int key;
+        Node* p;
+    public:
+        //сеттеры
+        void setP(Node* p) { this->p = p; }
+        void setKey(int key) { this->key = key; }
+        //геттеры
+        Node* getP() { return p; }
+        int getKey() { return key; }
+
+    };
+    int N; //фиксируем кол-во узлов в стеке (по сути не самый обязательный параметр, 
+    //т.к. все можно проверить через vector.size(), но пусть будет для логики стека
+    std::vector<Node*> stack; //вектор узлов стека
+
+public:
+    //сеттеры
+    void setN(int N) { this->N = N; }
+    void setStack(std::vector<Node*> stack) { this->stack = stack; }
+
+    //геттеры
+    int getN() { return N; }
+    std::vector<Node*> getStackCopy() { return stack; }
+
+    //ОСНОВНОЙ ФУНКЦИОНАЛ КЛАССА
+    //сообщение, что стек пуст
+    void stackIsEmpty() { std::cout << "----------" << "STACK IS EMPTY" << "----------" << std::endl; }
+
+    //сообщение, что стек переполнен (в данной программе - если кол-во узлов будет превышать 15)
+    void stackIsOverflow() { std::cout << "----------" << "STACK IS OVERFLOW" << "----------" << std::endl; }
+
+    //вывести в терминал информацию о размере стека
+    void size() { std::cout << "----------" << "STACK SIZE IS " << N << "----------" << std::endl; }
+
+    //добавить узел в стек
+    void push() {
+        if (N >= 15)
+            stackIsOverflow();
+        else {
+            Node* obj = new Node;
+            (*obj).setP(obj);
+            (*obj).setKey(N);
+            stack.push_back(obj);
+            N++;
+        }
+    }
+    //вывести весь стек в терминал
+    void print() {
+        if (N == 0)
+            stackIsEmpty();
+        else {
+            for (std::vector<Node*>::reverse_iterator iter = stack.rbegin(); iter < stack.rend(); iter++)
+                std::cout << "::::::::::" << (*(*iter)).getKey() << "::::::::::" << std::endl;
+        }
+    }
+    //удалить узел из стека
+    void pop() {
+        if (N <= 0)
+            stackIsEmpty();
+        else {
+            std::cout << "----------" << "DELETED NODE IS " << (*stack[N - 1]).getKey() << "----------" << std::endl;
+            stack.pop_back();
+            print();
+            N--;
+        }
+    }
+    //удалить весь стек
+    void remove() {
+        for (int i{ 0 }; i < stack.size(); i++)
+            delete (*stack[i]).getP();
+    }
+
+    //перегружаем операцию присваивания для создании копии стека
+    //тут момент в том, что если просто скопировать вектор,
+    //то в скопированном будут указатели на те же ячейки памяти, которые используются в объекте, который мы копировали,
+    //и тогда деструктор дважды попытается очистить одну и ту же область памяти. хотя ошибка при этом не вылезает, 
+    //все равно при копировании выделяем другую область памяти, но переносим значения узлов
+    void operator = (Stack& obj) {
+        stack.resize(obj.getN());
+        for (int i{ 0 }; i < obj.getN(); i++) {
+            if (stack[i] == nullptr) {
+                Node* new_obj = new Node;
+                (*new_obj).setP(new_obj);
+                stack[i] = new_obj;
+            }
+            (*stack[i]).setKey((*obj.stack[i]).getKey());
+        }
+        N = obj.N;
+        std::cout << "----------COPIED STACK----------" << std::endl;
+        print();
+        std::cout << "----------" << "COPIED STACK SIZE IS " << N << "----------" << std::endl;
+    }
+
+    //инверсия (доп задание)
+    void reverse() {
+        Node* temp;
+        std::vector<Node*>::iterator iter_begin = stack.begin();
+        std::vector<Node*>::iterator iter_end = stack.end() - 1;
+
+        while (iter_end - iter_begin >= 1) {
+            temp = *iter_end;
+            *iter_end = *iter_begin;
+            *iter_begin = temp;
+
+            iter_end--;
+            iter_begin++;
+        }
+        std::cout << "----------REVERSED STACK----------" << std::endl;
+        print();
+    }
+    //перегрузка оператора (сложение стеков + стек с неповторяющимися значениями. доп)
+    void operator || (Stack& obj) {
+        std::cout << "----------SUM OF STACKS----------" << std::endl;
+        for (int i{ 0 }; i < obj.N; i++) {
+            stack.push_back((obj.getStackCopy())[i]);
+        }
+        N += obj.N;
+        print();
+
+        std::cout << "----------STACK WITH UNIQUE ELEMENTS----------" << std::endl;
+        for (int i{ 0 }; i < N; i++) {
+            for (int j{ i+1 }; j < N; j++) {
+                if ((*stack[i]).getKey() == (*stack[j]).getKey()) {
+                    stack.erase(stack.begin() + j);
+                    j--;
+                    N--;
+                }
+            }
+        }
+        print();
+    }
+
+    //деструктор
+    ~Stack() {
+        std::cout << "DESTRUCTION (работа деструктора)." << std::endl;
+        remove();
+    }
+
+    //конструктор дефолтный
+    Stack() : N{ 10 } {
+        Node* obj;
+        for (int i{ 0 }; i < N; i++) {
+            obj = new Node;
+            (*obj).setP(obj);
+            (*obj).setKey(i);
+            stack.push_back(obj);
+        }
+    }
+    //конструктор недефолтный
+    Stack(int N) {
+        Node* obj;
+        if (N > 15 || N < 0)
+            this->N = 10;
+        else
+            this->N = N;
+        for (int i{ 0 }; i < N; i++) {
+            obj = new Node;
+            (*obj).setP(obj);
+            (*obj).setKey(i);
+            stack.push_back(obj);
+        }
+    }
+};
--- extra3_stack/extra3_stack.cpp
+++ extra3_stack/extra3_stack.cpp
@@ -1,6 +1,4 @@
-﻿#include <iostream>
-#include <vector>
-#include <algorithm>
+﻿#include "classStack.h"
 
 //СТЕК - структура организации памяти (структура данных), при которой первыми удаляются те данные, которые были записаны последними.
 //обычно используется в функциях.
@@ -17,153 +15,21 @@
 //распределение в нем происходит так, что левый потомок всегда имеет меньшее значение, чем свой родитель, а правый - всегда большее.
 //используется, например, в контейнере map.
 
-class Stack {
-//приватные атрибуты
-private:
-    class Node {
-    private:
-        int key;
-        Node* p;
-    public:
-        //сеттеры геттеры
-        void setP(Node* p) { this->p = p; }
-        Node* getP() { return p; }
-        int getKey() { return key; }
-        void setKey(int key) { this->key = key; }
-    };
-    int N;
-    std::vector<Node*> stack;
-
-public:
-    //сеттеры геттеры
-    void setN(int N) { this->N = N; }
-    int getN() { return N; }
-
-    std::vector<Node*> getStackCopy() { return stack; }
-    void setStack(std::vector<Node*> stack) { this->stack = stack; }
-    //основной функционал класса
-    void stackIsEmpty() { std::cout << "----------" << "STACK IS EMPTY" << "----------" << std::endl; }
-    void stackIsOverflow() { std::cout << "----------" << "STACK IS OVERFLOW" << "----------" << std::endl; }
-    void size() { std::cout << "----------" << "STACK SIZE IS " << stack.size() << "----------" << std::endl; }
-
-    void push() {
-        if (N >= 15)
-            stackIsOverflow();
-        else {
-            Node* obj = new Node;
-            (*obj).setP(obj);
-            (*obj).setKey(N);
-            stack.push_back(obj);
-            setN(++N);
-        }
-    }
-
-    void print() {
-        if (N == 0)
-            stackIsEmpty();
-        else {
-            for (std::vector<Node*>::reverse_iterator iter = stack.rbegin(); iter < stack.rend(); iter++)
-                std::cout << "::::::::::" << (*(*iter)).getKey() << "::::::::::" << std::endl;
-        }
-    }
-
-    void pop() {
-        if (N <= 0)
-            stackIsEmpty();
-        else {
-            std::cout << "----------" << "DELETED NODE IS " << (*stack[getN() - 1]).getKey() << "----------" << std::endl;
-            stack.pop_back();
-            print();
-            setN(--N);
-        }
-    }
-
-    void remove() {
-        for (int i{ 0 }; i < stack.size(); i++)
-            delete (*stack[i]).getP();
-    }
-    //инверсия (доп задание)
-    void reverse() {
-        Node* temp;
-        std::vector<Node*>::iterator iter_begin = stack.begin();
-        std::vector<Node*>::iterator iter_end = stack.end()-1;
-
-        while (iter_end - iter_begin >= 1) {
-            temp = *iter_end;
-            *iter_end = *iter_begin;
-            *iter_begin = temp;
-
-            iter_end--;
-            iter_begin++;
-        }
-        std::cout << "----------REVERSED STACK----------" << std::endl;
-        print();
-    }
-    //перегрузка оператора (сложение стеков + стек с неповторяющимися значениями. доп)
-    void operator || (Stack &obj) {
-        std::cout << "----------SUM OF STACKS----------" << std::endl;
-        for (int i{ 0 }; i < (obj.getStackCopy()).size(); i++) {
-            stack.push_back((obj.getStackCopy())[i]);
-        }
-        setN(N + obj.getStackCopy().size());
-        print();
-
-        std::cout << "----------STACK WITH UNIQUE ELEMENTS----------" << std::endl;
-        for (int i{ 0 }; i < stack.size()-1; i++) {
-            for(int j{i+1}; j < stack.size(); j++){
-                if ((*stack[i]).getKey() == (*stack[j]).getKey()) {
-                    stack.erase(stack.begin() + j);
-                    j--;
-                    setN(--N);
-                }
-            }
-    }
-        print();
-    }
-    //деконструктор
-    ~Stack() {
-        std::cout << "DESTRUCTION (работа деструктора)." << std::endl;
-        remove();
-    }
-    //конструкторы
-    Stack() : N{ 10 } {
-        Node* obj;
-        for (int i{ 0 }; i < N; i++) {
-            obj = new Node;
-            (*obj).setP(obj);
-            (*obj).setKey(i);
-            stack.push_back(obj);
-        }
-    }
-
-    Stack(int N) {
-        Node* obj;
-        if (N > 15 || N < 0)
-            this->N = 10;
-        else
-            this->N = N;
-
-        for (int i{ 0 }; i < N; i++) {
-            obj = new Node;
-            (*obj).setP(obj);
-            (*obj).setKey(i);
-            stack.push_back(obj);
-        }
-    }
-};
-
-
 int main()
 {
     setlocale(LC_ALL, "rus");
     srand(time(NULL));
 
     int N;
-    //преамбула
     std::cout << "Введите кол-во элементов в стеке (не больше 15 для одного стека, иначе будет переполнен!): ";
     std::cin >> N;
     
+    //все необходимые объекты (основной стек, стек для копирования, стек для сложения)
     Stack ctuck(N);
+    Stack* stack_copy = new Stack(0);
+    Stack new_one(1 + rand() % 15);
+
+    //ПРЕАМБУЛА
     std::cout << "0 - Выйти из программы. " << std::endl;
     std::cout << "1 - Сообщение о том, что стек пуст." << std::endl;
     std::cout << "2 - Сообщение о том, что стек переполнен." << std::endl;
@@ -174,9 +40,10 @@ int main()
     std::cout << "7 - Вывести информацию о длине стека." << std::endl;
     std::cout << "8 - Вывести стек в терминал. " << std::endl;
     
-    //реализация функционала класса
+ 
+    //ВЫВЕДЕНИЕ ФУНКЦИОНАЛА КЛАССА В КОНСОЛЬ
     while (1){
-        Stack* stack_ptr = new Stack();
+        
         std::cout << "Введите цифру: ";
         std::cin >> N;
         switch (N)
@@ -185,15 +52,15 @@ int main()
         {
             std::cout << "Бонус! Сейчас вам будут представлены два сложенные стека (один, который только что использовался," << std::endl;
             std::cout << "а второй с рандомным количеством элементов), а так же полученный стек только с уникальными элементами и его инвертированная версия." << std::endl;
-            Stack new_one(1 + rand() % 15);
-
+            
             ctuck || new_one;
             ctuck.reverse();
             std::cout << std::endl;
 
             std::cout << "Спасибо за то, что выбрали именно нашу программу!" << std::endl;
             std::cout << std::endl << "---------------------------------------" << std::endl;
-            delete stack_ptr;
+            delete stack_copy;
+
             return 0;
         }
         case 1:
@@ -203,10 +70,8 @@ int main()
             ctuck.stackIsOverflow();
             break;
         case 3:
-        {
-            (*stack_ptr).setStack(ctuck.getStackCopy());
-            std::cout << "COPIED STACK:" << std::endl;
-            (*stack_ptr).print();
+        {  
+            *stack_copy = ctuck;
             break;
         }
         case 4:
@@ -220,7 +85,6 @@ int main()
         {
             std::cout << "Бонус! Сейчас вам будут представлены два сложенные стека (один, который только что использовался," << std::endl;
             std::cout << "а второй с рандомным количеством элементов), а так же полученный стек только с уникальными элементами и его инвертированная версия." << std::endl;
-            Stack new_one(1 + rand() % 15);
 
             ctuck || new_one;
             ctuck.reverse();
@@ -229,7 +93,8 @@ int main()
             ctuck.stackIsEmpty();
             std::cout << "Спасибо за то, что выбрали именно нашу программу!" << std::endl;
             std::cout << std::endl << "---------------------------------------" << std::endl;
-            delete stack_ptr;
+            delete stack_copy;
+            
             return 0;
         }
         case 7:
@@ -242,7 +107,6 @@ int main()
         {
             std::cout << "Бонус! Сейчас вам будут представлены два сложенные стека (один, который только что использовался," << std::endl;
             std::cout << "а второй с рандомным количеством элементов), а так же полученный стек только с уникальными элементами и его инвертированная версия." << std::endl;
-            Stack new_one(1 + rand() % 15);
 
             ctuck || new_one;
             ctuck.reverse();
@@ -250,7 +114,8 @@ int main()
 
             std::cout << "Такой цифры не было среди предложенных." << std::endl;
             std::cout << std::endl << "---------------------------------------" << std::endl;
-            delete stack_ptr;
+            delete stack_copy;
+           
             return 0;
         }
             
